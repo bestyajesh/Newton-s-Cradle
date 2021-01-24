8569331a75175ecdbcd8b64ab9f19657559f01c5
Besty <67360804+bestyajesh@users.noreply.github.com>
2021-01-24 16:04:25 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,7 +3,6 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
-const Render = Matter.Render;
 const Constraint = Matter.Constraint;
 var bobObject1,bobObject2,bobObject3, bobObject4,bobObject5, roofObject
 var rope1,rope2,rope3, rope4,rope5;
@@ -31,22 +30,8 @@ function setup() {
 	bobObject5=new bob(startBobPositionX+bobDiameter*2,startBobPositionY,bobDiameter);
 	
 	
-	//Create a Ground
 	
-
-	var render = Render.create({
-	  element: document.body,
-	  engine: engine,
-	  options: {
-	    width: 1200,
-	    height: 700,
-	    wireframes: false
-	  }
-	});
-
-
 	rope1=new rope(bobObject1.body,roofObject.body,-bobDiameter*2, 0)
-
 	rope2=new rope(bobObject2.body,roofObject.body,-bobDiameter*1, 0)
 	rope3=new rope(bobObject3.body,roofObject.body,0, 0)
 	rope4=new rope(bobObject4.body,roofObject.body,bobDiameter*1, 0)
@@ -92,20 +77,6 @@ function keyPressed() {
 
   	}
 }
-
-
-function drawLine(constraint)
-{
-	bobBodyPosition=constraint.bodyA.position
-	roofBodyPosition=constraint.bodyB.position
-
-	roofBodyOffset=constraint.pointB;
-	
-	roofBodyX=roofBodyPosition.x+roofBodyOffset.x
-	roofBodyY=roofBodyPosition.y+roofBodyOffset.y
-	line(bobBodyPosition.x, bobBodyPosition.y, roofBodyX,roofBodyY);
-}
-
 
 
 
